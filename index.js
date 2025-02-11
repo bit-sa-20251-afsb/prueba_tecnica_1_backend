2747aa7f117b2ddc41469f3bdb3f4172a16371c1
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-02-11 18:22:47 -0500

    adding cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const api = require('./routes/api.routes');
+const cors = require('cors');
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 const connectDB = require('./config/database');
@@ -10,6 +11,7 @@ connectDB();
 // Inciar la corrida del servidor y escuchar en el puerto que indiqué
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 app.use('/',api);
 app.listen(PORT, () =>{
     console.log(`Escuchando en el punto de entrada: http://localhost:${PORT}`);
