3d311960b4dbf9325db2a54e8c9713faa7a39bb4
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-02-13 21:07:48 -0500

    inicio de test de login, fin de tests de createUser

--- __tests__/user.controller.test.js
+++ __tests__/user.controller.test.js
@@ -1,21 +1,28 @@
 // Importación falsa de la dependencia
 const User = require('../models/user.model');
-const {getAllUsers} = require('../controllers/user.controller');
-
+const {getAllUsers, createUser, loginUser} = require('../controllers/user.controller');
+const { generateToken } = require('../middlewares/jwtGenerate');
 // Mockeo de las depedendencias 
 jest.mock('../models/user.model');
+jest.mock('../middlewares/jwtGenerate');
 
 describe('Pruebas unitarias controlador usuarios', ()=>{
 
     // Arrange
     let req;
     let res;
+    let username;
+    let password;
+    let token;
     beforeEach(()=>{
         req = {params:{}, body:{}}
         res = { 
             status: jest.fn().mockReturnThis(), //Valor finito (primitivo - bool, number, string)
             json: jest.fn() // Valor complejo variable (no primitivo - objeto, array, Set)
         }
+        username = 'test';
+        password = 'test';
+        token = 'token-generado-jwt';
     })
 
     describe('Tests de obtener los usuarios - GET USERS',()=> {
@@ -43,5 +50,93 @@ describe('Pruebas unitarias controlador usuarios', ()=>{
             expect(res.json).toHaveBeenCalledWith({msg: 'Ocurrió un error'});
         })
     })
+
+    describe('Tests de crear un usuario - CREATE USER',()=>{
+        it('Deberia crear un usuario', async ()=>{
+            
+            // Arrange
+            req.body = {username, password};
+            User.findOne.mockResolvedValue(null);
+            User.prototype.save = jest.fn().mockResolvedValue(); // Verificación de que si se hizo el guardado
+            
+            // Act
+            await createUser(req,res);
+
+            // Assert
+            expect(User.findOne).toHaveBeenCalledWith({username});
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({
+                msg: `El usuario ${username} se creó correctamente.`
+            })
+        })
+
+        it('Deberia fallar la conexión a la base de datos', async ()=>{
+            // Arrange
+            req.body = {username, password}
+            User.findOne.mockRejectedValue(new Error('DB Error'));
+            // Act
+            await createUser(req,res);
+            // Assert
+            expect(User.findOne).toHaveBeenCalledWith({username})
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({
+                msg: 'Ocurrió un error inesperado'
+            })
+        })
+
+        it('Deberia obtener un error en caso que el usuario o contraseña esten vacios', ()=>{
+           
+            // Arrange
+
+            // Act
+            createUser(req,res);
+            // Assert
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({msg: 'Por favor ingrese los campos de usuario y contraseña'})
+        })
+
+        it('Deberia encontrar un usuario existente en base de datos', async ()=>{
+            // Arrange
+            req.body = {username, password};
+            User.findOne.mockResolvedValue({username,password});
+            // Act
+            await createUser(req,res);
+            // Assert
+            expect(User.findOne).toHaveBeenCalledWith({username});
+            expect(res.status).toHaveBeenCalledWith(409);
+            expect(res.json).toHaveBeenCalledWith({
+                msg: `El usuario ${username} ya existe. Por favor intente con otro usuario.`
+            }); 
+        })
+    })
+
+    describe('Tests para logear un usuario - LOGIN USER', ()=>{
+        it('Deberia ocurrir un error en caso de tener un usuario o contraseña', () => {
+            // Arrange
+
+            // Act
+            loginUser(req,res);
+            // Assert
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({msg: 'Por favor ingrese los campos de usuario y contraseña'})
+        })
+
+        it('Deberia logear el usuario correctamente', async ()=> {
+            // Arrange
+            req.body = {username,password};
+            User.findOne.mockResolvedValue({username,password});
+            generateToken.mockResolvedValue(token);
+            // Act
+            await loginUser(req,res);
+            // Assert
+            expect(User.findOne).toHaveBeenCalledWith({username});
+            expect(generateToken).toHaveBeenCalledWith({username,password});
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({
+                msg: `Bienvenido a la aplicación: ${username}`,
+                token: token
+            });
+        })
+    }) 
 
 })
